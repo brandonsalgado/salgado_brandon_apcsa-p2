2d62afa0d346ace0fdbb9f6e35684db2392ddb6e
brandonsalgado <bsalgado504@gmail.com>
2019-02-01 10:23:14 -0800

    Copied files over for unit 0

---
+++ Unit00/src/AsciiBox2.java
@@ -0,0 +1,29 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("name \t  date\n\n " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA" );
+		System.out.println("+++++++++++++++++++++++++" );
+		System.out.println("+++                   +++" );
+		System.out.println("+++                   +++" );
+		System.out.println("+++                   +++" );
+		System.out.println("+++                   +++" );
+		System.out.println("+++                   +++" );
+		System.out.println("+++     Comp Sci      +++" );
+		System.out.println("+++                   +++" );
+		System.out.println("+++                   +++" );
+		System.out.println("+++                   +++" );
+		System.out.println("+++                   +++" );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA" );
+		System.out.println("+++++++++++++++++++++++++" );
+	}
+}
