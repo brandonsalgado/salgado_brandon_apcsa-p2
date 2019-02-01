bba76ce9433dbe4c2cb48654d07d7a956680ff2e
brandonsalgado <bsalgado504@gmail.com>
2019-02-01 11:08:31 -0800

    Completed Unit1 StarsAndStripes

---
+++ Unit01/src/StarsAndStripes.java
@@ -0,0 +1,45 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public void StarsAndStripesI()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+	   out.println("********************");
+   }
+
+   public void printTwentyDashes()
+   {
+	   out.println("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   out.println();
+	   out.println();
+   }
+   
+   public void printASmallBox()
+   {	
+	   
+   }
+ 
+   public void printABigBox()
+   { 	
+   }
+
+   
+}
---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,44 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+	  StarsAndStripes ref = new StarsAndStripes();
+      //instantiate a StarsAndStripes object
+      ref.StarsAndStripesI();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwoBlankLines();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+
+
+      //call the methods needed to make the patterns on the word document
+      
+   }
+}
