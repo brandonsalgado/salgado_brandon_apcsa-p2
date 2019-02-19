e5ae508016db1df7968451b61add95b8848165b7
brandonsalgado <bsalgado504@gmail.com>
2019-02-19 11:12:53 -0800

    Finished triples in Unit07

--- Unit07/src/Triples.java
+++ Unit07/src/Triples.java
@@ -7,6 +7,7 @@ import static java.lang.System.*;
 public class Triples
 {
    private int number;
+   private int gcd;
 
 	public Triples()
 	{
@@ -15,22 +16,29 @@ public class Triples
 
 	public Triples(int num)
 	{
-
+		setNum(num);
 
 	}
 
 	public void setNum(int num)
 	{
-
+		number = num;
 
 	}
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
 		int max = 0;
-
-
-
+		
+		for (int m = 1; m <= a && m <= b && m <= c; m++)
+		{
+			if (a%m == 0 && b%m == 0 && c%m == 0)
+			{
+				gcd = m;
+			}
+			
+		}
+		
 		return 1;
 	}
 
@@ -38,9 +46,33 @@ public class Triples
 	{
 		String output="";
 
+		for (int i = 1; i < number; i++)
+		{
+			for (int j = 1; j < number; j++)
+			{
+				for (int k = 1; k < number; k++)
+				{
+					if ((i*i) + (j*j) == (k*k))
+					{
+						if (k%2 != 0)
+						{
+							if ((i%2 == 0 && j%2 != 0)||(i%2 != 0 && j%2 == 0))
+							{
+								greatestCommonFactor(i, j, k);
+								if (gcd == 1)
+								{
+									output += i + " " + j + " " + k + "\n";
+								}
+							}
 
-
-
+						}
+						
+					
+				}
+			}
+		}
+		
+		}
 
 
 		return output+"\n";
--- Unit07/src/TriplesRunner.java
+++ Unit07/src/TriplesRunner.java
@@ -4,9 +4,18 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class TriplesRunner
 {
    public static void main(String args[])
    {
+	   Scanner keyboard = new Scanner(System.in);
+	   
+	   out.println("Enter number: ");
+	   int num = keyboard.nextInt();
+	   
+	   Triples trip = new Triples(num);
+	   out.println(trip);
 	}
 }
