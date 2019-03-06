fc55f615097e96d3387c811b3ef74ea1392e9e67
brandonsalgado <bsalgado504@gmail.com>
2019-03-06 11:11:10 -0800

    Finished Unit09 activities 3&4 and started activity5

--- Unit09/src/activity4/DeckTester.java
+++ Unit09/src/activity4/DeckTester.java
@@ -1,4 +1,4 @@
-package activity2;
+package activity4;
 
 import java.util.Arrays;
 
@@ -13,13 +13,13 @@ public class DeckTester {
 	 */
 	public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-		String[] ranks = {"five", "six", "seven", "eight"};
-		String[] suits = {"diamonds", "spades"};
-		int[] val = {5, 6, 7, 8};
+		String[] ranks = {"ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"};
+		String[] suits = {"diamonds", "spades", "hearts", "clubs"};
+		int[] val = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 		
 		Deck deck1 = new Deck(ranks, suits, val);
 		
-		String[] ranks2 = {"nine", "ten"};
+		/*String[] ranks2 = {"nine", "ten"};
 		String[] suits2 = {"hearts", "spades"};
 		int[] val2 = {9, 10};
 		
@@ -30,14 +30,21 @@ public class DeckTester {
 		int[] val3 = {11, 12, 13};
 		
 		Deck deck3 = new Deck(ranks3, suits3, val3);
-		
+		*/
 				
 		
 		System.out.println(deck1);
 		System.out.println(deck1.size());
 		System.out.println(deck1.isEmpty());
 		System.out.println(deck1.deal());
-		System.out.println(deck2);
+		
+		
+		
+		
+		
+		
+		
+		/*System.out.println(deck2);
 		System.out.println(deck2.size());
 		System.out.println(deck2.isEmpty());
 		System.out.println(deck2.deal());
@@ -51,7 +58,7 @@ public class DeckTester {
 		System.out.println(deck3.deal());
 		System.out.println(deck3.deal());
 		System.out.println(deck3.isEmpty());
-		
+		*/
 		
 	}
 }
