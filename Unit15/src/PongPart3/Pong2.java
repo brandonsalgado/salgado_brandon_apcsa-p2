432f05a85081c35718161a1e813901f824979199
brandonsalgado <bsalgado504@gmail.com>
2019-05-06 10:15:05 -0700

    Finished the pong game extension(in unit15 java project)

--- Unit15/src/PongPart3/Pong2.java
+++ Unit15/src/PongPart3/Pong2.java
@@ -13,31 +13,70 @@ import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import static java.lang.Character.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.awt.event.ActionListener;
 
 public class Pong2 extends Canvas implements KeyListener, Runnable
 {
 	private Paddle leftPaddle;
  	private boolean[] keys;
+	private Ball ball;
 	private BufferedImage back;
 	private int leftscore;
-
+	private ArrayList<Block> blocks;
+	private static int level;
 
 	public Pong2()
 	{
+		level++;
 		//set up all variables related to the game
 		leftscore = 0;
 		leftPaddle = new Paddle(300, 300, 40, 40, 5);
-
+		ball = new Ball();
 
 		keys = new boolean[4];
-
-    
+		
+		blocks = new ArrayList<Block>();
+		if (level == 1)
+		{
+		for (int i = 0; i < 4; i++)
+		{
+			blocks.add(new Block(80 + (i*150), 0, 125, 30));
+			blocks.add(new Block(80 + (i*150), 70, 125, 30));
+			blocks.add(new Block(80 + (i*150), 400, 125, 30));
+			blocks.add(new Block(80 + (i*150), 470, 125, 30));
+		}
+		for (int i = 0; i < 2; i++)
+		{
+			blocks.add(new Block(10, 120 + (i*150), 30, 125));
+			blocks.add(new Block(60, 120 + (i*150), 30, 125));
+			blocks.add(new Block(650, 120 + (i*150), 30, 125));
+			blocks.add(new Block(710, 120 + (i*150), 30, 125));
+		}
+		}
+		if (level == 2)
+		{
+			for (int i = 0; i < 8; i++)
+			{
+				blocks.add(new Block(80 + (i*75), 0, 62, 30));
+				blocks.add(new Block(80 + (i*75), 70, 62, 30));
+				blocks.add(new Block(80 + (i*75), 400, 62, 30));
+				blocks.add(new Block(80 + (i*75), 470, 62, 30));
+			}
+			for (int i = 0; i < 4; i++)
+			{
+				blocks.add(new Block(10, 120 + (i*75), 30, 62));
+				blocks.add(new Block(60, 120 + (i*75), 30, 62));
+				blocks.add(new Block(650, 120 + (i*75), 30, 62));
+				blocks.add(new Block(710, 120 + (i*75), 30, 62));
+			}
+		}
     	setBackground(Color.WHITE);
 		setVisible(true);
 		
 		new Thread(this).start();
 		addKeyListener(this);		//starts the key thread to log key strokes
+
 	}
 	
    public void update(Graphics window){
@@ -58,61 +97,107 @@ public class Pong2 extends Canvas implements KeyListener, Runnable
 		//we will draw all changes on the background image
 		Graphics graphToBack = back.createGraphics();
 
-		//graphToBack.drawString("Right Score: " + rightscore, 500, 500);
-		graphToBack.drawString("Left Score: " + leftscore, 100, 500);
-		//ball.moveAndDraw(graphToBack);
+		graphToBack.drawString("Level: " + level, 10, 10);
+		//graphToBack.drawString("Left Score: " + leftscore, 100, 500);
+		ball.moveAndDraw(graphToBack);
 		leftPaddle.draw(graphToBack);
+		for (int i = 0; i < blocks.size(); i++)
+		{
+			blocks.get(i).draw(graphToBack);
+		}
 
+		for (int i = 0; i < blocks.size(); i++)
+		{
+			if (ball.getX() > blocks.get(i).getX() && ball.getX() < blocks.get(i).getX() + blocks.get(i).getWidth())
+			{
+				if (ball.getY() < blocks.get(i).getY() + blocks.get(i).getHeight() && ball.getY() > blocks.get(i).getY())
+				{
+					//hits tile
+
+					if (blocks.get(i).getHeight() > blocks.get(i).getWidth())
+					{
+						ball.setXS(-ball.getXS());
+					}
+					else
+					{
+					ball.setYS(-ball.getYS());
+					}
+					blocks.get(i).draw(graphToBack, Color.white);
+					blocks.remove(i);
+					if (blocks.size() == 0)
+					{
+						//finished level
+						PongGameExtension run2 = new PongGameExtension();
+						//level = 2;
+						
+					}
+				}
+			}
+		}
+		
 		
 		//see if ball hits left wall or right wall
-		/*if(!(ball.getX()>=10))
+		if(!(ball.getX()>=10))
 		{
+			/*
 			graphToBack.setColor(Color.white);
 		    graphToBack.fillRect(ball.getX(), ball.getY(), ball.getWidth(), ball.getHeight());
 			ball.setX(200);
 			ball.setY(200);
 			graphToBack.setColor(Color.white);
-			graphToBack.drawString("Right Score: " + rightscore, 200, 500);
-			rightscore++;
+			//graphToBack.drawString("Right Score: " + rightscore, 200, 500);
+			//rightscore++;
+			*/
+			ball.setXS(-ball.getXS());
+
 		}
 		if(!(ball.getX()<=780))
 		{
-			graphToBack.setColor(Color.white);
+			/*graphToBack.setColor(Color.white);
 		    graphToBack.fillRect(ball.getX(), ball.getY(), ball.getWidth(), ball.getHeight());
 			ball.setX(200);
 			ball.setY(200);
 			graphToBack.setColor(Color.white);
 			graphToBack.drawString("Left" + leftscore, 200, 500);
 			leftscore++;
+			*/
+			ball.setXS(-ball.getXS());
 		}
 		
 		//see if the ball hits the top or bottom wall 
-		if(!(ball.getY()>=10 && ball.getY()<=450))
+		if(!(ball.getY()>=10 && ball.getY()<=550))
 		{
 			ball.setYS(-ball.getYS());
 		}
 
 
 		//see if the ball hits the left paddle
-		if ((ball.getY() < leftPaddle.getY() + 40 && ball.getY() > leftPaddle.getY())&&(ball.getX() == leftPaddle.getX() + 2))
+		if ((ball.getY() + ball.getHeight() < leftPaddle.getY() + leftPaddle.getHeight() && ball.getY() > leftPaddle.getY())&&(ball.getX() > leftPaddle.getX() && ball.getX() + ball.getWidth() < leftPaddle.getX() + leftPaddle.getWidth()))
 		{
-			ball.setXS(-ball.getXS());
+			if (ball.getX() > leftPaddle.getX() + ball.getWidth() && ball.getX() < leftPaddle.getX() + leftPaddle.getWidth() - ball.getWidth())
+			{
+				ball.setYS(-ball.getYS());
+			}
+			else
+			{
+				ball.setXS(-ball.getXS());
+			}
 		}
 		
 		
 		//see if the ball hits the right paddle
 		
-		*/
+		
 
 
 		//see if the paddles need to be moved
 
-		if(keys[1] == true && leftPaddle.getY() < 450)
+		if(keys[1] == true && leftPaddle.getY() < 520)
 		{
 			//move left paddle up and draw it on the window
 			leftPaddle.moveDownAndDraw(graphToBack);
 		}
-		if(keys[0] == true && leftPaddle.getY() > 10)
+		if(keys[0] == true && leftPaddle.getY() > 0)
 		{
 			//move left paddle down and draw it on the window
 			leftPaddle.moveUpAndDraw(graphToBack);
@@ -121,7 +206,7 @@ public class Pong2 extends Canvas implements KeyListener, Runnable
 		{
 			leftPaddle.moveLeftAndDraw(graphToBack);
 		}
-		if (keys[3] == true && leftPaddle.getX() < 700)
+		if (keys[3] == true && leftPaddle.getX() < 745)
 		{
 			leftPaddle.moveRightAndDraw(graphToBack);
 		}
